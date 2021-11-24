0efc2898a9f20dfcaed47b5a47716dbac2d95e47
PRKKILLER <36620963+PRKKILLER@users.noreply.github.com>
2021-11-24 15:50:15 -0800

    Spring init

---
+++ backend/VaccineResevationSystem/src/main/java/com/example/vaccineresevationsystem/VaccineResevationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.vaccineresevationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccineResevationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccineResevationSystemApplication.class, args);
+	}
+
+}
---
+++ backend/VaccineResevationSystem/src/test/java/com/example/vaccineresevationsystem/VaccineResevationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vaccineresevationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccineResevationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
